ec13cb93ac551c48b6f6dfb08f6614adbd098b45
Nguyen Hoang Son <nhson@apcs.vn>
2019-04-19 03:09:38 +0700

    boring-channel

--- main.go
+++ main.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
-type Ball struct{ hits int }
-
 func main() {
-	table := make(chan *Ball)
-	go player("pong", table)
-	go player("ping", table)
-	time.Sleep(1 * time.Second)
-
-	table <- new(Ball) // game on; toss the ball
-	time.Sleep(1 * time.Second)
-	<-table // game over; grab the ball
+	c := make(chan string)
+	go boring("boring!", c)
+	for i := 0; i < 5; i++ {
+		fmt.Printf("You say: %q\n", <-c) // Receive expression is just a value.
+	}
+	fmt.Println("You're boring; I'm leaving.")
 }
 
-func player(name string, table chan *Ball) {
-	for {
-		ball := <-table
-		ball.hits++
-		fmt.Println(name, ball.hits)
-		time.Sleep(100 * time.Millisecond)
-		table <- ball
+func boring(msg string, c chan string) {
+	for i := 0; ; i++ {
+		c <- fmt.Sprintf("%s %d", msg, i) // Expression to be sent can be any suitable value.
+		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 	}
 }
