734d52bbca8d9bd4d6ace647cc976dbc60a94efd
Nguyen Hoang Son <nhson@apcs.vn>
2019-04-19 03:53:45 +0700

    fanin using select

--- main.go
+++ main.go
@@ -29,12 +29,12 @@ func fanIn(input1, input2 <-chan string) <-chan string {
 	c := make(chan string)
 	go func() {
 		for {
-			c <- <-input1
-		}
-	}()
-	go func() {
-		for {
-			c <- <-input2
+			select {
+			case s := <-input1:
+				c <- s
+			case s := <-input2:
+				c <- s
+			}
 		}
 	}()
 	return c
