26af07401cdf8f7deb1c8c0c2f3aa32eac4e5f6d
Nguyen Hoang Son <nhson@apcs.vn>
2019-04-19 01:36:31 +0700

    pingpong

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Ball struct{ hits int }
+
+func main() {
+	table := make(chan *Ball)
+	go player("pong", table)
+	go player("ping", table)
+	time.Sleep(1 * time.Second)
+
+	table <- new(Ball) // game on; toss the ball
+	time.Sleep(1 * time.Second)
+	<-table // game over; grab the ball
+}
+
+func player(name string, table chan *Ball) {
+	for {
+		ball := <-table
+		ball.hits++
+		fmt.Println(name, ball.hits)
+		time.Sleep(100 * time.Millisecond)
+		table <- ball
+	}
+}
